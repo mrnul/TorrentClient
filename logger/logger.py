2d8adbdb28156cfbf8a5799d302d2b0555a3d689
mrnull <kiriakidis.pm@gmail.com>
2024-03-09 17:14:03 +0200

    Add logging handler only if it does not exist

--- logger/logger.py
+++ logger/logger.py
@@ -7,8 +7,9 @@ class Logger:
 
     def get(self, log_file: str, name: str, level: str = 'INFO') -> logging.Logger:
         logger = logging.getLogger(name)
-        handler = logging.FileHandler(log_file)
-        handler.setFormatter(self.formatter)
-        logger.addHandler(handler)
-        logger.setLevel(level)
+        if not logger.hasHandlers():
+            handler = logging.FileHandler(log_file, mode='w')
+            handler.setFormatter(self.formatter)
+            logger.addHandler(handler)
+            logger.setLevel(level)
         return logger
